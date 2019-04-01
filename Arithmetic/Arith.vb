f29ccb4ab05815893d1262f7a8f27c2d9c6fbae9
Ariel Alejandro Wagner <31016611+arielwagner@users.noreply.github.com>
2019-04-01 18:12:22 -0300

    Add files via upload

---
+++ Arithmetic/Arith.vb
@@ -0,0 +1,50 @@
+﻿Public Class Arith
+
+    Public Function addition(ParamArray values() As Double) As Double
+        Try
+            Dim result As Double = 0
+            For Each vl As Double In values
+                result += vl
+            Next
+            Return result
+        Catch ex As Exception
+            Return 0
+        End Try
+    End Function
+
+    Public Function substraction(ParamArray values() As Double) As Double
+        Try
+            Dim result As Double = 0
+            For Each vl As Double In values
+                result -= vl
+            Next
+            Return result
+        Catch ex As Exception
+            Return 0
+        End Try
+    End Function
+
+    Public Function multiplication(ParamArray values() As Double) As Double
+        Try
+            Dim result As Double = 0
+            For Each vl As Double In values
+                result *= vl
+            Next
+            Return result
+        Catch ex As Exception
+            Return 0
+        End Try
+    End Function
+
+    Public Function division(ParamArray values() As Double) As Double
+        Try
+            Dim result As Double = 0
+            For Each vl As Double In values
+                result /= vl
+            Next
+            Return result
+        Catch ex As Exception
+            Return 0
+        End Try
+    End Function
+End Class
